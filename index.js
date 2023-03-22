06f5286b3dce7e5cdf51fa870235f9ec1cfce538
NikitaCat <kit-2003@mail.ru>
2023-03-22 23:35:20 +0300

    routersIndex

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config(); // конфиг для env 
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+
+const router = require('./routes/index');
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Work!" });
+});
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => { console.log(`Успешный запуск на порте: ${PORT}`); });
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+
+start();
